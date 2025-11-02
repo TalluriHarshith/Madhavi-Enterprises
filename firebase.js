1d8f9f69060aac991e4ab4c1f7f14e6a7e92b8af
Talluri Harshith <talluriharshit2006@gmail.com>
2025-11-02 15:59:27 +0530

    admin

--- firebase.js
+++ firebase.js
@@ -1,23 +1,15 @@
-
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js';
-import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHUpNiZoB7_gO7NVmns3bhsWMPSg6xULM",
-  authDomain: "enterprices-website.firebaseapp.com",
-  projectId: "enterprices-website",
-  storageBucket: "enterprices-website.appspot.com",
-  messagingSenderId: "371964783835",
-  appId: "1:371964783835:web:e8715517a5bfd5a87af0fe",
-  measurementId: "G-HW1NE4P807"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
 };
 
 const app = initializeApp(firebaseConfig);
-let analytics;
-try { analytics = getAnalytics(app); } catch (e) { /* ignore when unavailable */ }
-
-export const auth = getAuth(app);
 export const db = getFirestore(app);
-export default app;
